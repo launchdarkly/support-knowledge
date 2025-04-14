95ffd1d32fd79bcfa0c42d5262e4e6f046194499
Menelik Zafir <mzafir@launchdarkly.com>
2025-04-14 17:52:52 -0400

    fix: moved the constant outside the scope of the Welcome function,

--- examples/React-Native/src/welcome.tsx
+++ examples/React-Native/src/welcome.tsx
@@ -1,9 +1,12 @@
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { useBoolVariation } from '@launchdarkly/react-native-client-sdk';
 
+//TODO Set my-boolean-flag to a valid boolean flag key in your project/environment.
+const FLAG_KEY = 'my-boolean-flag';
+
 export default function Welcome() {
-  //TODO Set my-boolean-flag to a valid boolean flag key in your project/environment.
-  const flagValue = useBoolVariation('bool-flag', false);
+  const flagValue = useBoolVariation(FLAG_KEY, false);
 
   return (
     <View style={styles.container}>
