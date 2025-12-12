d0463b340e84e46fbb075987743cc18cfaf1d0c6
Menelik Zafir <mzafir@professorice.com>
2025-12-12 11:01:51 -0500

    [TSS-1081] Next.js example (#13)

---
+++ examples/hello-nextjs/app/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { useFlags } from 'launchdarkly-react-client-sdk';
+
+export default function Page() {
+  const flags = useFlags();
+  //TODO Set my-boolean-flag to a valid boolean flag key in your project/environment.
+  const value = flags['my-boolean-flag'] ?? false;
+
+  return (
+    <main className="container">
+      <h1>Welcome to LaunchDarkly</h1>
+      <h1>Flag value is {String(value)}</h1>
+    </main>
+  );
+}
